80173ba736c81ae71b44828a31af01f9bb1ad6db
Dan Kochetov <danil.kochetov@gmail.com>
2023-01-13 13:39:11 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import initSqlJs, { SqlValue } from 'sql.js';
+
+function App() {
+	const [now, setNow] = useState<SqlValue>();
+
+	useEffect(() => {
+		(async () => {
+			const SQL = await initSqlJs({
+				// Required to load the wasm binary asynchronously. Of course, you can host it wherever you want
+				// You can omit locateFile completely when running in node
+				locateFile: (file) => `https://sql.js.org/dist/${file}`,
+			});
+
+			const db = new SQL.Database();
+
+			const { now } = db
+				.prepare("select datetime('now') as now")
+				.getAsObject({});
+
+			setNow(now);
+		})().catch((e) => console.error(e));
+	}, []);
+
+	if (!now) return <div>Loading...</div>;
+
+	return <div>Current time from SQLite: {now}</div>;
+}
+
+export default App;
